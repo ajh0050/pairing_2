b1e203fad712f33b6176980100595d7edeb031f5
Adam Hughes <adamjordanhughes@gmail.com>
2022-08-03 16:37:07 -0400

    Initial commit

---
+++ friends.js
@@ -0,0 +1,10 @@
+var friend = "lucas"
+
+if (friend === "lucas") {
+  console.log("good day lucas");
+} else {
+  consoole.log("good day other friend")
+}
+
+// console.log(friend);
+// console.log(me);
